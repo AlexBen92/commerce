6cb0ff673f9f633f8f6c83b9a28dc4b7c6f32d2f
Alex <alex@gmail.com>
2021-03-24 07:19:50 +0100

    Updates

---
+++ precache-manifest.5928fd2fe7788141fac236fc91e42aab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeec1087149c14d9465fca5cf7f60cf4",
+    "url": "/AlexBen92/shop.git/index.html"
+  },
+  {
+    "revision": "13e4c7c5505a981466ec",
+    "url": "/AlexBen92/shop.git/static/css/2.8af765a2.chunk.css"
+  },
+  {
+    "revision": "245968700983f50f1db6",
+    "url": "/AlexBen92/shop.git/static/css/main.790ddf4c.chunk.css"
+  },
+  {
+    "revision": "13e4c7c5505a981466ec",
+    "url": "/AlexBen92/shop.git/static/js/2.a7a0b953.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AlexBen92/shop.git/static/js/2.a7a0b953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245968700983f50f1db6",
+    "url": "/AlexBen92/shop.git/static/js/main.ee5fde8b.chunk.js"
+  },
+  {
+    "revision": "d3ff2a511b252dac0a2f",
+    "url": "/AlexBen92/shop.git/static/js/runtime-main.164dbadf.js"
+  },
+  {
+    "revision": "0210e597f574b4bd3f751ca6b3629f1c",
+    "url": "/AlexBen92/shop.git/static/media/bbb.0210e597.jpg"
+  },
+  {
+    "revision": "59731bae1af505b797e0bab856268623",
+    "url": "/AlexBen92/shop.git/static/media/dslr.59731bae.jpg"
+  },
+  {
+    "revision": "8d318e4a591da8b33dddbe1467a809c9",
+    "url": "/AlexBen92/shop.git/static/media/iphone.8d318e4a.jpg"
+  },
+  {
+    "revision": "e7bdef88d0ff67525ebeb96877270e62",
+    "url": "/AlexBen92/shop.git/static/media/microphone.e7bdef88.jpg"
+  },
+  {
+    "revision": "92ef6ab7b31320d776128b8bf7944473",
+    "url": "/AlexBen92/shop.git/static/media/perfume.92ef6ab7.jpg"
+  },
+  {
+    "revision": "986af3717a208f0b993fffa0d353abe6",
+    "url": "/AlexBen92/shop.git/static/media/rings.986af371.jpg"
+  },
+  {
+    "revision": "f30fe0b2e59c7281ecf5bd07d202a595",
+    "url": "/AlexBen92/shop.git/static/media/shoes.f30fe0b2.jpg"
+  },
+  {
+    "revision": "b4e58e91e391491d2836ce808e677037",
+    "url": "/AlexBen92/shop.git/static/media/watch.b4e58e91.jpg"
+  }
+]);
